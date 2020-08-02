4dd9f80b7d21f7df81c17f0c271a309aaf5df6f7
Blake Williams <code@shabbyrobe.org>
2020-08-02 21:41:35 +1000

    Get GitHub Actions going again

--- tools.sh
+++ tools.sh
@@ -3,19 +3,19 @@ set -o errexit -o nounset -o pipefail
 script_abspath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cmd-deploy() {
-    source .venv/bin/activate
+    source venv/bin/activate
     python3 setup.py sdist bdist_wheel
     python3 -m twine upload dist/*
 }
 
 cmd-dev-setup() {
-    python3 -m venv .venv
-    source .venv/bin/activate
+    python3 -m venv venv
+    source venv/bin/activate
     pip install -r dev-requirements.txt
 }
 
 cmd-test() {
-    source .venv/bin/activate
+    source venv/bin/activate
 
     pushd "$script_abspath/grpc-stubs" > /dev/null
         mypy -- *.pyi
