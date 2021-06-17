7ff8a7840e8a22c59c66faecda5115998ac05b4d
Teran McKinney <Teran.McKinney@anthem.com>
2021-06-17 14:18:57 -0600

    Bump mypy requirement to 0.902

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 __version__ = "1.24.6"
 
 dependencies = [
-    'mypy>=0.730',
+    'mypy>=0.902',
     "typing-extensions; python_version<'3.8'",
     'grpcio',
 ]
