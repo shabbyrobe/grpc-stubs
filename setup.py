6d38b08a648719de032ba41cf76eb0d96020e2de
Blake Williams <code@shabbyrobe.org>
2023-12-27 15:52:38 +1100

    1.53.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 from setuptools import find_packages
 from distutils.core import setup
 
-__version__ = "1.53.0.3"
+__version__ = "1.53.0.4"
 
 dependencies = [
     'grpcio',
